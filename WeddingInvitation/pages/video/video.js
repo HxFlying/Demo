02824ce09e84d80106246399a3e3f15509ab59e0
HxFlying <963091583@qq.com>
2019-10-21 09:09:50 +0800

    婚礼邀请帖

---
+++ WeddingInvitation/pages/video/video.js
@@ -0,0 +1,82 @@
+// pages/video/video.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  
+    data: {
+      movieList: [{
+        create_time: 1532519754589,
+        title: '海边随拍',
+        src: 'http://uvideo.spriteapp.cn/video/2019/1015/5da53ca7baaa7_wpd.mp4'
+
+    }, {
+          create_time: 1532519777690,
+          title: '勿忘心安',
+          src: 'http://uvideo.spriteapp.cn/video/2019/1015/5da5805f73f55_wpd.mp4'
+
+    }, {
+          create_time: 1532519734589,
+          title: '点滴记忆',
+          src: 'http://uvideo.spriteapp.cn/video/2019/0802/5d43cc3ff2ddf_wpd.mp4'
+    }]
+    }
+ ,
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
